84111decb816b0ee35033eb677187d5b597e1b7b
Nadeem Qasmi <nadeemqasmi40@yahoo.com>
2019-10-13 20:23:11 +0500

    Update config.js
    update

--- config.js
+++ config.js
@@ -11,14 +11,6 @@ module.exports = {
             database: process.env.database || 'youcan',
         },
         
-        databaseRemote : {
-            dialect: process.env.database_dialect || 'postgres',
-            host: process.env.database_host || 'youcav-db.cnfabpkcjfhv.us-east-1.rds.amazonaws.com' ,
-            username: process.env.database_username || 'postgres' ,
-            password: process.env.database_password || 'Qwerty12',
-            database: process.env.database || 'youcan',
-            schema: process.env.schema || 'activityservice',
-            elasticsearch_host: process.env.elasticsearch || 'http://172.17.0.1:9200'
-        },
+      
  
 }
